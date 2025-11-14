189591d7dce234c053da533b74d80298335e8596
zz4206 <zz4206@nyu.edu>
2025-11-14 03:12:50 -0500

    api key set up

--- machine-learning-client/audio.py
+++
@@ -1,7 +0,0 @@
-from azure.cognitiveservices.speech import SpeechConfig, AudioConfig
-import azure.cognitiveservices.speech as speechsdk
-from config import AZURE_SPEECH_KEY, AZURE_SERVICE_REGION
-
-speech_config = SpeechConfig(subscription=AZURE_SPEECH_KEY, region=AZURE_SERVICE_REGION)
-audio_config = AudioConfig(use_default_microphone=True)
-speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, language="en-US", audio_config=audio_config)
